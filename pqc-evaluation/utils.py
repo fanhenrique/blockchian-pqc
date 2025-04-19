16efda97ff0f74f8fef6e34682fbd6a3bc95547e
Henrique Fan <fanhenrique@gmail.com>
2025-04-19 01:12:51 -0300

    positive int

--- pqc-evaluation/utils.py
+++ pqc-evaluation/utils.py
@@ -1,5 +1,25 @@
+import argparse
+
 def one_level(df_all, level, graphics):
     for graphic in graphics:
         if level != graphic['level']:
             df_all = df_all.drop([i for i in graphic['mechanisms']])
     return df_all
+
+def positive_int(value: int):
+    """
+    Validates that the provided value is a positive integer.
+
+    Parameters:
+        value (int): The value to validate.
+
+    Returns:
+        int: The validated positive integer.
+
+    Raises:
+        argparse.ArgumentTypeError: If the value is not a positive integer.
+    """
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(f"{value} is not a positive integer")
+    return ivalue
