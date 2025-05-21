c440b8743ad9cf5c8bc506729a19564f2a72bd41
Henrique Fan <fanhenrique@gmail.com>
2025-05-21 20:15:31 -0300

    print variants

--- pqc-evaluation/main.py
+++ pqc-evaluation/main.py
@@ -107,6 +107,19 @@ def run_sizes(mechanisms, sizes_evaluation):
     return pd.DataFrame(results_sizes)
 
 
+def print_variants(mechanisms, oqs_mechanisms, normalize):
+
+    mechanism_groups = load_mechanisms(
+        input_mechanisms=mechanisms.keys(),
+        oqs_get_mechanisms=oqs_mechanisms,
+        normalize=normalize
+    )
+
+    for mechanism, variants in mechanism_groups.items():
+        print(f"{mechanism}:")
+        for variant in variants:
+            print(f"  - {variant}")
+
 def main():
 
     parser = argparse.ArgumentParser(
@@ -116,12 +129,34 @@ def main():
     parser.add_argument('--kem', help='List KEM algorithms', type=str, nargs='+', choices=list(KEM_MECHANISMS.keys()))
     parser.add_argument('--sig', help='List signature algorithms', type=str, nargs='+', choices=list(SIG_MECHANISMS.keys()))
     # TODO ainda não funciona, vai implementar
-    # parser.add_argument('--levels', '-l', help='Nist level', type=int, choices=range(1, 6), nargs='+')
-    parser.add_argument('--number', '-n', help='Number of the execution', type=utils.positive_int, default=1)
+    parser.add_argument("--list_kem", help="List of variants KEM algorithms", action="store_true")
+    parser.add_argument("--list_sig", help="List of variants digital signature algorithms", action="store_true")
     
     args = parser.parse_args()
 
     print(args)
+
+    if args.list_kem:
+        print("List of KEM algorithm variants")
+        print(oqs.get_enabled_kem_mechanisms())
+        # print(oqs.get_supported_kem_mechanisms())
+
+        print_variants(
+            mechanisms=KEM_MECHANISMS,
+            oqs_mechanisms=oqs.get_enabled_kem_mechanisms,
+            normalize=KEM_MECHANISMS
+        )
+
+    if args.list_sig:
+        print("Digital Signature ")
+        print(oqs.get_enabled_sig_mechanisms())
+        # print(oqs.get_supported_sig_mechanisms())
+
+        print_variants(
+            mechanisms=SIG_MECHANISMS,
+            oqs_mechanisms=oqs.get_enabled_sig_mechanisms,
+            normalize=SIG_MECHANISMS
+        )
 
     if args.kem:
 
